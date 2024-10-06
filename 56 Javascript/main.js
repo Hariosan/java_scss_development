ca08414e7649ad2836d82214745d54c5a857ce8f
yuma <yhario@icloud.com>
2024-10-06 16:45:20 +0900

    Add files via upload

---
+++ 56 Javascript/main.js
@@ -0,0 +1,37 @@
+//let=changable const=unchangable
+
+let name = 'Tom';
+
+name = 'Tim';
+
+console.log("Hello" + name);
+
+let variable = 'str';
+
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof "variable");
+
+/*
+let variable = 'str'; <---string (Its just an example. ofc you can you others like undefiend and null instead of string depends on the situation below.)
+
+variable = 12;
+
+If you wanna show 
+
+words sentences<---then use "string"
+
+Number <---number
+
+True False shape <---Boolean 
+
+Undefined the number will be undefined (In this case, 12 will be undefined. If you use undefined there.)
+
+Null
+
+console.log(typeof variable);
+
+You can check what variable are used by using above code 
+*/
