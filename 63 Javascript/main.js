4f36706aef9c77f4a26817d8c201859441cd864b
yuma <yhario@icloud.com>
2024-10-13 21:40:46 +0900

    Add files via upload

---
+++ 63 Javascript/main.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        id: 1,
+        title: "Go to School",
+        completed: true
+    },
+    {
+        id: 2,
+        title: "Go to Museum",
+        completed: true
+    },
+    {
+        id: 3,
+        title: "Go Shopping",
+        completed: false
+    }
+    
+]
+
+for(let i = 0; i < todos.length; i++){
+    if(todo.completed === true){
+    console.log(i, todo.title);
+    }
+}
